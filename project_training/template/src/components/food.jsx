3d57ce6cf1fc92458f1e823a3ea13455059248b1
ans9611 <62779373+ans9611@users.noreply.github.com>
2022-03-31 22:05:24 -0400

    button instruction

--- project_training/template/src/components/food.jsx
+++ project_training/template/src/components/food.jsx
@@ -7,6 +7,15 @@ class Food extends Component {
             <>
                 <div>MY favorite food is</div>
                 <span className='food-name'>Texas BBQ</span>
+                <button>
+                    <i class="fa-solid fa-circle-plus"></i>
+                </button>
+                <button>
+                    <i class="fa-solid fa-circle-minus"></i>
+                </button>
+                <button>
+                    <i class="fa-solid fa-delete-left"></i>
+                </button>
                 <span className='food-name'>Texas BBQ</span>
             </>
         )
